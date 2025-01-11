117973b5137456604dcf5cf813f2ae51a71bad11
Anditia Putra <123170083+anditiaputra@users.noreply.github.com>
2025-01-11 19:47:23 +0700

    rm pw

--- esp8266_emqx_mqtt.ino
+++ esp8266_emqx_mqtt.ino
@@ -2,22 +2,21 @@
 #include <PubSubClient.h>
 #include <WiFiClientSecure.h>
 
-#define ledPin 2 // GPIO 2 (D4) for LED
-#define relay1Pin 4
-#define relay2Pin 5
+#define relay1Pin 4    // GPIO4 (D2)
+#define relay2Pin 5    // GPIO5 (D1)
 
 bool ledState = false;
 
 // WiFi settings
-const char *ssid = "ADYAKIDS";       // Replace with your WiFi name
-const char *password = "Uprut2024";  // Replace with your WiFi password
+const char *ssid = "ssidwifi";                   // WiFi name
+const char *password = "passwordwifi";           // WiFi password
 
 // MQTT Broker settings
-const char *mqtt_broker = "ic1ebc99.ala.asia-southeast1.emqxsl.com";  // EMQX broker endpoint
-const char *mqtt_topic = "pci/gerbang";                               // MQTT topic
-const char *mqtt_username = "pci";                                    // MQTT username for authentication
-const char *mqtt_password = "Puricemaraindah2025";                    // MQTT password for authentication
-const int mqtt_port = 8883;                                           // MQTT port (TCP)
+const char *mqtt_broker = "abc.emqxsl.com";      // MQTT broker
+const char *mqtt_topic = "abc/topic";            // MQTT topic
+const char *mqtt_username = "username";          // MQTT username for authentication
+const char *mqtt_password = "password";          // MQTT password for authentication
+const int mqtt_port = 8883;                      // MQTT port (TCP)
 
 WiFiClientSecure espClient;
 PubSubClient mqtt_client(espClient);
@@ -28,10 +27,8 @@ void mqttCallback(char* topic, byte* payload, unsigned int length);
 
 void setup() {
     Serial.begin(115200);
-
-    pinMode(ledPin, OUTPUT);          // Set LED as an output
-    pinMode(relay1Pin, OUTPUT);    // Set relay1Pin as an output
-    pinMode(relay2Pin, OUTPUT);    // Set relay2Pin as an output
+    pinMode(relay1Pin, OUTPUT);
+    pinMode(relay2Pin, OUTPUT);
 
     connectToWiFi();
     mqtt_client.setServer(mqtt_broker, mqtt_port);
@@ -89,12 +86,6 @@ void mqttCallback(char* topic, byte* payload, unsigned int length) {
         Serial.println("Turning on relay 2 (tutup)");
         delay(2000);
         digitalWrite(relay2Pin, LOW);
-    } else if (message == "ledON") {
-        digitalWrite(ledPin, HIGH); // Turn on relay 2 (tutup gerbang)
-        Serial.println("Turning on led");
-    } else if (message == "ledOFF") {
-        digitalWrite(ledPin, LOW); // Turn on relay 2 (tutup gerbang)
-        Serial.println("Turning off led");
     } else {
         Serial.println("Unknown command: " + message);
     }
